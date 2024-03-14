2d53b5295498f18072e7becb1989d7148af95aed
Naimur Islam <90306726+Naiml007@users.noreply.github.com>
2024-03-14 19:36:11 +0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+// imports
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+//starts http server
+func main() {
+	go func() {
+		http.HandleFunc("/", getRoot)
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "Inferno is at render.com now.. 🚀\n")
+}
